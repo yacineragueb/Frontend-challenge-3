4f9f81f9755dcf46fe3065b726f71c1e44d653aa
yacine <yacineddd32@gmail.com>
2025-03-05 22:33:32 +0100

    age calculator app starts

---
+++ age-calculator-app-main/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, sapiente
+      impedit quis voluptatum exercitationem eveniet sit ratione voluptas
+      laborum repellendus, tempore amet omnis quos ipsum blanditiis voluptatem
+      porro, veritatis ut!
+    </div>
+  );
+}
