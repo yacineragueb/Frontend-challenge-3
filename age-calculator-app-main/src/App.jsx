e9da7ea59e3df22f463b0986c3c31544f5b97d4b
yacine <yacineddd32@gmail.com>
2025-03-06 23:21:40 +0100

    validateInputs almost done

--- age-calculator-app-main/src/App.jsx
+++ age-calculator-app-main/src/App.jsx
@@ -1,19 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./Components/Input";
 
+function validateInputs(obj, setErrors) {
+  let newErrors = {};
+  let date = new Date();
+
+  if (!obj.day) newErrors.day = "This field is required";
+  if (!obj.month) newErrors.month = "This field is required";
+  if (!obj.year) newErrors.year = "This field is required";
+
+  if (obj.day && (obj.day < 1 || obj.day > 31))
+    newErrors.day = "Must be a valid day";
+  if (obj.month && (obj.month < 1 || obj.month > 12))
+    newErrors.month = "Must be a valid month";
+  if (obj.year && obj.year > date.getFullYear())
+    newErrors.year = "Must be in the past";
+
+  if (Object.keys(newErrors).length > 0) {
+    setErrors((prevErrors) => ({ ...prevErrors, ...newErrors }));
+    return;
+  }
+
+  return true;
+}
+
 export default function App() {
+  const [userDate, setUserDate] = useState({ day: "", month: "", year: "" });
+  const [errors, setErrors] = useState({ day: "", month: "", year: "" });
+
+  const handleOnChange = (name, value) => {
+    if (isNaN(value)) {
+      setErrors((prev) => ({ ...prev, [name]: "Please enter a number." }));
+    } else {
+      setErrors((prev) => ({ ...prev, [name]: "" }));
+    }
+    setUserDate((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleClick = () => {
+    let isValid = validateInputs(userDate, setErrors);
+
+    if (!isValid) return;
+  };
+
   return (
     <section className=" px-6 py-8 md:p-10 overflow-hidden bg-white mt-20 md:mt-0 w-[90%] mx-auto rounded-t-3xl rounded-bl-3xl rounded-br-[5rem] md:rounded-br-[10rem] md:w-170">
       <div className="flex flex-col">
         <div className="flex space-x-4 md:w-[80%]">
-          <Input type={"DAY"} placeholder={"DD"}/>
-          <Input type={"MONTH"} placeholder={"MM"}/>
-          <Input type={"YEAR"} placeholder={"YYYY"}/>
+          <Input
+            label={"DAY"}
+            placeholder={"DD"}
+            name={"day"}
+            value={userDate.day}
+            onChange={handleOnChange}
+            error={errors.day}
+          />
+          <Input
+            label={"MONTH"}
+            placeholder={"MM"}
+            name={"month"}
+            value={userDate.month}
+            onChange={handleOnChange}
+            error={errors.month}
+          />
+          <Input
+            label={"YEAR"}
+            placeholder={"YYYY"}
+            name={"year"}
+            value={userDate.year}
+            onChange={handleOnChange}
+            error={errors.year}
+          />
         </div>
 
         <div className="relative h-[2px] bg-light-grey my-10 md:mt-8 md:mb-12">
-          <div className="bg-purple w-14 md:w-20 h-14 md:h-20 rounded-full grid place-items-center absolute top-1/2 -translate-y-1/2 left-1/2 md:left-auto md:right-0 -translate-x-1/2 md:translate-x-0 transition-colors duration-200 hover:bg-black cursor-pointer">
-            <img src="/images/icon-arrow.svg" alt="arrow" className="w-6 md:w-10"/>
+          <div
+            onClick={handleClick}
+            className="bg-purple w-14 md:w-20 h-14 md:h-20 rounded-full grid place-items-center absolute top-1/2 -translate-y-1/2 left-1/2 md:left-auto md:right-0 -translate-x-1/2 md:translate-x-0 transition-colors duration-200 hover:bg-black cursor-pointer"
+          >
+            <img
+              src="/images/icon-arrow.svg"
+              alt="arrow"
+              className="w-6 md:w-10"
+            />
           </div>
         </div>
 
--- age-calculator-app-main/src/Components/Input.jsx
+++ age-calculator-app-main/src/Components/Input.jsx
@@ -1,17 +1,26 @@
-import React from "react";
-
-export default function Input({type, placeholder}) {
+export default function Input({
+  label,
+  placeholder,
+  name,
+  value,
+  onChange,
+  error,
+}) {
   return (
     <div className="flex flex-col gap-2 flex-1 min-w-0">
-      <span className="text-smokey-grey font-bold tracking-[3px]">{type}</span>
+      <label className={`${error ? 'text-light-red' : 'text-smokey-grey'} font-bold tracking-[3px]`}>
+        {label}
+      </label>
       <input
         type="text"
+        value={value}
         placeholder={placeholder}
-        className="border p-4 rounded-lg border-light-grey font-bold text-[2rem] outline-none transition-colors duration-200 focus:border-purple caret-purple"
+        onChange={(e) => onChange(name, e.target.value)}
+        className={`border p-3 md:p-4 rounded-lg ${error ? 'border-light-red focus:border-light-red caret-light-red' : 'border-light-grey focus:border-purple caret-purple'} font-bold text-xl md:text-[2rem] outline-none transition-colors duration-200`}
       />
-      <p className="text-light-red text-[15px] font-italic">
-        this field is required
-      </p>
+      {error && (
+        <p className="text-light-red text-[15px] font-italic">{error}</p>
+      )}
     </div>
   );
 }
