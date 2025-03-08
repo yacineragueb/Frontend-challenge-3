ae7c509d99994efb820cda1329dfa1eb2df9b78f
yacine <yacineddd32@gmail.com>
2025-03-08 14:03:51 +0100

    fylo landing page challenge start

---
+++ fylo-landing-page-with-two-column-layout-master/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+      animi perspiciatis ipsa. Unde nam neque minus. Quos cupiditate sint eius,
+      quibusdam architecto repellat ad repellendus voluptas accusamus modi
+      laudantium asperiores!
+    </div>
+  );
+}
