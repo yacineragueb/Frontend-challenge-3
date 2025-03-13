2a84297c96be849a8883ae15510982e8ed8223f6
yacine <yacineddd32@gmail.com>
2025-03-13 18:42:41 +0100

    this is just the begining of challenge

--- advice-generator-app-main/src/App.jsx
+++ advice-generator-app-main/src/App.jsx
@@ -1,7 +1,19 @@
-
-
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <section>
+      <h5>ADVICE #177</h5>
+      <p>
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat maxime
+        praesentium nam a amet recusandae repellat."
+      </p>
+      <picture>
+        <source
+          srcSet="/images/pattern-divider-desktop.svg"
+          type="image/svg+xml"
+          media="(min-width: 640px)"
+        />
+        <img src="/images/pattern-divider-mobile.svg" alt="divider" />
+      </picture>
+    </section>
+  );
 }
